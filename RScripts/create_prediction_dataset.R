ea379f812bc72484124acc8958e40ef85692d4fa
Aakash Singh <aakashsingh717@gmail.com>
2024-12-01 13:25:00 -0500

    SD the num columns in predict

--- RScripts/create_prediction_dataset.R
+++ RScripts/create_prediction_dataset.R
@@ -3,20 +3,39 @@ library(dplyr)
 
 # Read the CSV file
 input_file <- "./DataSet/cleaned_accepted_2013_to_2018Q4.csv"  # Replace with the path to your CSV file
-data <- fread(input_file)
+df <- fread(input_file)
 
 # Convert issue_d to Date format
-data$issue_d <- as.Date(data$issue_d, format = "%Y-%m-%d")
+df$issue_d <- as.Date(df$issue_d, format = "%Y-%m-%d")
 
 # Filter data based on the conditions
-filtered_data <- data %>%
+df <- df %>%
   select(issue_d, loan_amnt, int_rate, grade, sub_grade, emp_length, annual_inc, 
          dti, fico_range_low, fico_range_high, last_fico_range_high, 
          last_fico_range_low, open_acc_6m, loan_status) %>%
   filter(issue_d > as.Date("2015-01-01") & loan_status == "Current")
 
+df$sub_grade <- substr(df$sub_grade, 2, nchar(df$sub_grade))
+df$grade <- factor(df$grade)
+df$sub_grade <- factor(df$sub_grade)
+df$emp_length <- factor(df$emp_length)
+df$loan_status <- factor(df$loan_status)
+
+numerical_columns <- c(
+  "loan_amnt", "int_rate", "annual_inc", "dti", 
+  "fico_range_low", "fico_range_high", 
+  "last_fico_range_low", "last_fico_range_high", 
+  "open_acc_6m"
+)
+
+# Standardize numerical columns
+df[, (numerical_columns) := lapply(.SD, scale), .SDcols = numerical_columns]
+
+
+
+
 # Save the filtered data to a new CSV file
 output_file <- "predict.csv"  # Replace with your desired output file path
-write.csv(filtered_data, output_file, row.names = FALSE)
+write.csv(df, output_file, row.names = FALSE)
 
 cat("Filtered data saved to", output_file)
